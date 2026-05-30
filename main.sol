86cb60effd8cfc12f60cc3710eb8413e21600da5
Pnutbro88b <ko5588070@gmail.com>
2026-05-30 13:17:37 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -55,3 +55,60 @@ library JPLDigestFork {
 contract JPevinLoop {
     address public immutable ADDRESS_A;
     address public immutable ADDRESS_B;
+    address public immutable ADDRESS_C;
+
+    bytes32 private constant JPL_DOMAIN_SALT = keccak256("JPevinLoop.meridian_trough");
+    bytes16 private constant JPL_SEED = 0x7c3e91b4d8f02a651e9c4b7d3a1f8065;
+    uint64 public constant JPL_BUILD_TAG = 0x9A4E2C71B8D03F56;
+    uint32 public constant JPL_BUILD_STAMP = 1847293156;
+
+    uint64 public constant MAX_POOL_ID = 847_291;
+    uint32 public constant MAX_STRATEGY_BPS = 9_847;
+    uint32 public constant MAX_PERF_FEE_BPS = 2_500;
+    uint32 public constant MAX_MGMT_FEE_BPS = 500;
+    uint32 public constant MAX_ROUTE_BYTES = 384;
+    uint32 public constant MAX_BATCH = 56;
+    uint32 public constant MAX_SNAPSHOT_RING = 128;
+    uint32 public constant HARVEST_COOLDOWN_SEC = 3_601;
+    uint256 public constant MIN_DEPOSIT_WEI = 512;
+    uint256 public constant MIN_HARVEST_TIP_WEI = 271;
+    uint8 public constant POOL_STATUS_DORMANT = 0;
+    uint8 public constant POOL_STATUS_LIVE = 1;
+    uint8 public constant POOL_STATUS_RETIRED = 2;
+
+    struct YieldPool {
+        bytes32 strategyRoot;
+        bytes32 routeTag;
+        address curatorNote;
+        uint8 status;
+        bool harvestOpen;
+        bool sealed;
+        uint64 openedAt;
+        uint64 closesAt;
+        uint32 harvestCount;
+        uint32 depositorCount;
+        uint256 totalAssetsWei;
+        uint256 totalShares;
+        uint256 tipReserve;
+    }
+
+    struct DepositorSeat {
+        bytes32 intentHash;
+        bytes32 lastHarvestTag;
+        bool active;
+        uint64 joinedAt;
+        uint256 shares;
+        uint256 creditedWei;
+        uint64 lastHarvestAt;
+    }
+
+    struct HarvestRing {
+        bytes32 yieldProof;
+        bytes32 apyBlend;
+        address reporter;
+        uint64 poolId;
+        uint64 recordedAt;
+        uint32 apySampleBps;
+        bool revoked;
+    }
+
