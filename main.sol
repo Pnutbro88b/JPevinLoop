8076a5a0bf91811929b1c92c8944e241440b2530
Pnutbro88b <ko5588070@gmail.com>
2026-05-31 03:32:16 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -1480,3 +1480,60 @@ contract JPevinLoop {
         assets = new uint256[](n);
         shares = new uint256[](n);
         for (uint256 j = 0; j < n; ++j) {
+            YieldPool storage p = _pools[poolIds[j]];
+            statuses[j] = p.status;
+            assets[j] = p.totalAssetsWei;
+            shares[j] = p.totalShares;
+        }
+    }
+    function batchPoolStatus_5(uint64[] calldata poolIds)
+        external
+        view
+        returns (uint8[] memory statuses, uint256[] memory assets, uint256[] memory shares)
+    {
+        uint256 n = poolIds.length;
+        if (n > MAX_BATCH) revert JPL_BatchTooLarge(n, MAX_BATCH);
+        statuses = new uint8[](n);
+        assets = new uint256[](n);
+        shares = new uint256[](n);
+        for (uint256 j = 0; j < n; ++j) {
+            YieldPool storage p = _pools[poolIds[j]];
+            statuses[j] = p.status;
+            assets[j] = p.totalAssetsWei;
+            shares[j] = p.totalShares;
+        }
+    }
+    function batchPoolStatus_6(uint64[] calldata poolIds)
+        external
+        view
+        returns (uint8[] memory statuses, uint256[] memory assets, uint256[] memory shares)
+    {
+        uint256 n = poolIds.length;
+        if (n > MAX_BATCH) revert JPL_BatchTooLarge(n, MAX_BATCH);
+        statuses = new uint8[](n);
+        assets = new uint256[](n);
+        shares = new uint256[](n);
+        for (uint256 j = 0; j < n; ++j) {
+            YieldPool storage p = _pools[poolIds[j]];
+            statuses[j] = p.status;
+            assets[j] = p.totalAssetsWei;
+            shares[j] = p.totalShares;
+        }
+    }
+    function batchPoolStatus_7(uint64[] calldata poolIds)
+        external
+        view
+        returns (uint8[] memory statuses, uint256[] memory assets, uint256[] memory shares)
+    {
+        uint256 n = poolIds.length;
+        if (n > MAX_BATCH) revert JPL_BatchTooLarge(n, MAX_BATCH);
+        statuses = new uint8[](n);
+        assets = new uint256[](n);
+        shares = new uint256[](n);
+        for (uint256 j = 0; j < n; ++j) {
+            YieldPool storage p = _pools[poolIds[j]];
+            statuses[j] = p.status;
+            assets[j] = p.totalAssetsWei;
+            shares[j] = p.totalShares;
+        }
+    }
